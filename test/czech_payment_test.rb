d26ed5439d54047295cd4126e443fffd1c53e784
Jana Dvorakova <jana.dvorakova@kraxnet.cz>
2013-07-31 15:35:22 +0200

    test for account with prefix

--- test/czech_payment_test.rb
+++ test/czech_payment_test.rb
@@ -12,4 +12,16 @@ class CzechPaymentTest < MiniTest::Unit::TestCase
 
     assert_equal 'SPD*1.0*ACC:CZ9555000000000810883001*AM:430.00*CC:CZK*X-VS:31030001*MSG:PLATBA ZA DOMENU', result
   end
+
+  def test_czech_payment_with_account_prefix
+    result = Rspayd::CzechPayment.generate_string(
+      :accountPrefix => '7755',
+      :accountNumber => '77628031',
+      :bankCode => '0710',
+      :amount => 500.00,
+      :vs => '1234567890'
+    )
+
+    assert_equal 'SPD*1.0*ACC:CZ5607100077550077628031*AM:500.00*CC:CZK*X-VS:1234567890', result
+  end
 end
